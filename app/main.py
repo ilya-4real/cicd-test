5e27d912089d54a5164683ce9497f5e24f929267
ilya-4real <ilya.chernishyov@gmail.com>
2024-05-09 18:05:42 +0200

    added second endpoint

--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def greet() -> dict[str, str]:
     return {"message": "hello world"}
+
+
+@app.get("/cicd_endpoint")
+async def new_endpoint():
+    return {"message": "this is new endpoint delivered via CICD!! congrats!"}
